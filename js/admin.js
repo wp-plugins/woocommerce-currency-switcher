7f56cc334c6556e62eccb9cd9273fd973e718221
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-06 21:03:22 +0000

    updated files to ver.1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-currency-switcher/trunk@1107163 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/admin.js
@@ -0,0 +1,7 @@
+jQuery(function() {
+    jQuery('.form-table .forminp #woocommerce_currency').parents('tr').remove();
+    jQuery('.form-table .forminp #woocommerce_currency_pos').parents('tr').remove();
+});
+
+
+

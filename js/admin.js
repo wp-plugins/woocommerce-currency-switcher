c5bdbe775f6fa44d1df0e3d4ac1d7e995104f6ee
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-06 16:36:53 +0000

    removing before updating ...
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-currency-switcher/trunk@1214206 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/admin.js
+++
@@ -1,5 +0,0 @@
-jQuery(function() {
-    jQuery('.form-table .forminp #woocommerce_currency').parents('tr').remove();
-    jQuery('.form-table .forminp #woocommerce_currency_pos').parents('tr').remove();
-});
-
--- views/widgets/rates.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
-
-<div class="widget widget-woocommerce-currency-rates">
-    <?php if (!empty($instance['title'])): ?>
-        <h3><?php _e($instance['title']) ?></h3>
-    <?php endif; ?>
-    <?php echo do_shortcode('[woocs_rates exclude="' . $instance['exclude'] . '" precision="' . $instance['precision'] . '"]'); ?>
-</div>
-
